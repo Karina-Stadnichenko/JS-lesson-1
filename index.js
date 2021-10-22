52acd9d6d2bb17d077a08bdf537fb3e172bbca18
Karina Stadnichenko <felicesan9@gmail.com>
2021-10-22 19:34:09 +0300

    add console.log

--- index.js
+++ index.js
@@ -7,48 +7,65 @@
 //alert(message);
 
 //assignments 1
-let warning ;
+//let warning ;
 
-warning = "this site is for healthcare providers only";
+//warning = "this site is for healthcare providers only";
 
-alert(warning);
+//alert(warning);
 
-warning = "Are you sure you are a helthcare professional?";
+//warning = "Are you sure you are a helthcare professional?";
 
-alert(warning);
+//alert(warning);
 
 //////////////////
 
 //asignment 2
 
-let question;
+//let question;
 
-question = "How many numbers do you see?";
+//question = "How many numbers do you see?";
 
-alert(question);
+//alert(question);
 
-let numbers;
+//let numbers;
 
-numbers = 1488;
+//numbers = 1488;
 
-alert(numbers);
+//alert(numbers);
 
-alert(question + numbers);
+//alert(question + numbers);
 
 /////////////////////
 
 //asignment 3
 
-let string;
+//let string;
 
-string = "22";
+//string = "22";
 
-alert(string);
+//alert(string);
 
-let number;
+//let number;
 
-number = 2;
+//number = 2;
 
-alert(number);
+//alert(number);
 
-alert(string - number);
+//alert(string - number);
+
+const str = "stroka";
+console.log(str);
+
+const num = 2;
+console.log(num);
+
+//const bool = true;
+//console.log(bool);
+
+//const nullValue = null;
+//console.log(nullValue);
+
+//const undefinedValue = undefined;
+//console.log(undefinedValue);
+
+//console.log(1 + Number('nullValue'));
