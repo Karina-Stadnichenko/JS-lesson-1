440f80dcc2ebd574a543faa7fa51338e2405c849
Karina Stadnichenko <felicesan9@gmail.com>
2021-10-22 17:50:58 +0300

    add assignment

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-let message = {
-    name: undefined,
-    address: null,
-    age: 34,
-}; //Объявление переменной
+//const message = 'text'; //Объявление переменной
 
-alert( message.name );
+//alert( message );
+ 
+//message = 123;
 
-alert( message.address ); 
+//alert(message);
 
-alert( message.age ); 
+//assignments 1
+let warning ;
+
+warning = "this site is for healthcare providers only";
+
+alert(warning);
+
+warning = "Are you sure you are a helthcare professional?";
+
+alert(warning);
+
+//////////////////
+
+//asignment 2
+
+let question;
+
+question = "How many numbers do you see?";
+
+alert(question);
+
+let numbers;
+
+numbers = 1488;
+
+alert(numbers);
+
+alert(question + numbers);
+
+/////////////////////
+
+//asignment 3
+
+let string;
+
+string = "22";
+
+alert(string);
+
+let number;
+
+number = 2;
+
+alert(number);
+
+alert(string - number);
